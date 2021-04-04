f01e2acd16313eba8eb61a721847adc8ba00bca4
Ayush avasthi <avasthiayush22@gmail.com>
2021-04-04 22:52:25 +0530

    Updates

---
+++ precache-manifest.76cc19ba0a7c1fd646fb18fbd5e7918e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841337e33de8ee0f35d796be1e2abdff",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "f2b0d31ee7d8e65eb31a",
+    "url": "/spacex/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e800f7193746cb78532e",
+    "url": "/spacex/static/css/main.47ff2d3f.chunk.css"
+  },
+  {
+    "revision": "f2b0d31ee7d8e65eb31a",
+    "url": "/spacex/static/js/2.f703ff4a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/spacex/static/js/2.f703ff4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e800f7193746cb78532e",
+    "url": "/spacex/static/js/main.3718604a.chunk.js"
+  },
+  {
+    "revision": "38bd822f86f0fa518f7f",
+    "url": "/spacex/static/js/runtime-main.3f7de82f.js"
+  }
+]);
